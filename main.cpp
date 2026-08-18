95b16b44c50202e151fa9dd38e90a2716abb58bb
S.M. Rafiul Islam Arpon <rraafi00@gmail.com>
2026-08-18 20:56:58 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string name = "S. M. Rafiul Islam Arpon";
+    string id = "24-57662-2";
+    string section = "M";
+    string department = "CSE";
+    string university = "AIUB";
+  cout << "Student Information System" << endl;
+  cout << "Name: " << name << endl;
+  cout << "ID: " << id << endl;
+  cout << "Section: " << section << endl;
+  cout << "Department: " << department << endl;
+  cout << "University: " << university << endl;
+  cout << "Welcome to Git and GitHub!" << endl;
+  return 0;
+}
