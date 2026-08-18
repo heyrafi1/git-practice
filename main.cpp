a77e5d4ab3b589490ab5a566c4c0442333a93c50
S.M. Rafiul Islam Arpon <rraafi00@gmail.com>
2026-08-18 21:11:05 +0600

    Added favorite language field

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@ string name = "S. M. Rafiul Islam Arpon";
     string section = "M";
     string department = "CSE";
     string university = "AIUB";
+    string language = "C++";
   cout << "Student Information System" << endl;
   cout << "Name: " << name << endl;
   cout << "ID: " << id << endl;
   cout << "Section: " << section << endl;
   cout << "Department: " << department << endl;
   cout << "University: " << university << endl;
+  cout << "Favorite Language: " << language << endl;
   cout << "Welcome to Git and GitHub!" << endl;
   return 0;
 }
